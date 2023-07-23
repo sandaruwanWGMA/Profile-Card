a322fe4b17a0b8775da3e848ad239a9ba7a55a3c
Molindu Achintha <molinduachintha24@gmail.com>
2023-07-23 17:30:42 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import profilePicture from "./molindu copy.jpeg";
 
 function App() {
+    const imageStyles = {
+        width: 420,
+        height: 280
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="profile">
+      <img src={profilePicture} style={imageStyles}/>
+        <div id="intro">
+            <h1> Molindu Achintha</h1>
+        <p id="intro-para">
+            <strong>Machine learning enthusiastic</strong> and a <strong>web developer</strong>. I most like to
+            explore things by learning and my passion is to grab new knowledge
+            quickly. I am currently learning <strong>React</strong> and <strong>iOS development with swift </strong>
+            as well.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            <div className="s-1">
+                <p className= "s s--1">HTML + CSS 😎</p>
+                <p className= "s s--2">JavaSript 🫡</p>
+                <p className= "s s--3">Python ✊</p>
+            </div>
+
+            <div className="s-2">
+                <p className= "s s--4">Java 👍</p>
+                <p className= "s s--5">C++ 💪</p>
+            </div>
+        </div>
     </div>
   );
 }
